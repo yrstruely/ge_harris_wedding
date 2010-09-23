b5b542f585a0ec72565f28a67d6e512aaf8f2f11
Kerry Harris <kerry.harris0713@gmail.com>
2010-09-23 23:15:15 +0100

    Initil commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeHarrisWedding::Application.config.secret_token = '7d1548feb5d89d6aad65a6215dfea1e897c4f1f2a7a0420767fc25efd0c27a31c798fffc06dc7ac21f3f8d7abbb36ecf21a780cc39bd1f59030bc201c461bc05'
