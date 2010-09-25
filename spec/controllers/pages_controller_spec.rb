719a5cccf2da061fff74699e759582c6eef2fdce
Kerry Harris <kerry.harris0713@gmail.com>
2010-09-25 23:34:32 +0100

    partway through layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = 'Ge Harris Wedding'
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +15,7 @@ describe PagesController do
 
     it "should have the correct title" do
       get 'home'
-      response.should have_selector('title', :content => 'Ge Harris Wedding | Home')
+      response.should have_selector('title', :content => @base_title + ' | Home')
     end
   end
 
